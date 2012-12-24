17c4308d4b198464f58def5e871aa3b6b369d37e
Hendrik Helwich <hendrik@helwich.de>
2012-12-25 00:18:33 +0100

    move functions to super closure

--- src/jquery-justify.js
+++ src/jquery-justify.js
@@ -9,6 +9,134 @@
 (function ($) {
     'use strict';
 
+    /**
+     * Returns an array of the indices of the items which are the first in each row if the rows are breaking at
+     * the given maximum width. The length of the array is the number of rows which are needed.
+     *
+     * @param itemInfo
+     * @param maxWidth
+     * @param marginX
+     * @return {Array}
+     */
+    function getRowFirstItems(itemInfo, maxWidth, marginX) {
+        if (itemInfo.length === 0) {
+            return [];
+        }
+        var rowFirstItems = [ 0 ],
+            i,
+            rowWidth = 0,
+            itemStored = true, // current item index is already stored in the array rowFirstItems
+            itemWidth;
+        for (i = 0; i < itemInfo.length; i += 1) { // iterate all items
+            itemWidth = itemInfo[i].width;
+            rowWidth += itemWidth;
+            if (rowWidth > maxWidth) { // current item breaks max row width => break row
+                if (itemStored) {
+                    rowWidth = 0;
+                } else {
+                    rowFirstItems.push(i);
+                    rowWidth = itemWidth + marginX;
+                }
+            } else {
+                rowWidth += marginX;
+            }
+            itemStored = false;
+        }
+        return rowFirstItems;
+    }
+
+    /**
+     * Returns an array of the same semantic and length as getRowFirstItems() but the items are sectioned in
+     * an optimized way.
+     * The optimization tries to arrange the items in a more even way (uses binary search to come to a result
+     * fast)
+     *
+     * @param itemInfo
+     * @param maxWidth
+     * @param marginX
+     * @param accuracy
+     * @return {Array}
+     */
+    function getRowFirstItemsOptimized(itemInfo, maxWidth, marginX, accuracy) {
+        var dif = maxWidth,
+            firstBest = getRowFirstItems(itemInfo, maxWidth, marginX),
+            firstBestWidth = maxWidth,
+            firstCurrent = firstBest,
+            i;
+        for (i = 0; i < accuracy; i += 1) {
+            dif /= 2;
+            if (firstCurrent.length > firstBest.length) { // needed more rows => increase width
+                maxWidth += dif;
+            } else {
+                if (firstBestWidth > maxWidth) {
+                    firstBest = firstCurrent;
+                    firstBestWidth = maxWidth;
+                }
+                maxWidth -= dif;
+            }
+            firstCurrent = getRowFirstItems(itemInfo, maxWidth, marginX);
+        }
+        return firstBest;
+    }
+
+    /**
+     *
+     * @param itemInfo
+     * @param maxWidth
+     * @param settings
+     * @return {Array}
+     */
+    function createLineUp(itemInfo, maxWidth, settings) {
+        var firstRows = getRowFirstItemsOptimized(itemInfo, maxWidth, settings.marginX, settings.accuracy),
+            row,
+            col,
+            idx,
+            nextRowFirstIdx,
+            rowWidth = 0,
+            maxHeight = 0,
+            rowMarginX,
+            height = 0,
+            rowLength = firstRows.length,
+            lineup = [],
+            top,
+            left;
+
+        lineup.length = itemInfo.length; // expand array with undefined elements to known length (performance)
+
+        for (row = 0, idx = 0; row < rowLength; row += 1) { // iterate rows
+            nextRowFirstIdx = firstRows[row + 1];
+            if (nextRowFirstIdx === undefined) {
+                nextRowFirstIdx = itemInfo.length;
+            }
+            for (col = 0; idx < nextRowFirstIdx; idx += 1, col += 1) { // iterate row elements
+                // maxHeight: maximum height of all items in current row (type int)
+                maxHeight = Math.max(itemInfo[idx].height, maxHeight);
+                // rowWith: summed width of all items in current row (type int)
+                rowWidth += itemInfo[idx].width;
+            }
+            // calculate marginX value for current row to stretch the row to the container width (type float)
+            rowMarginX = ((maxWidth - rowWidth) / (col - 1));
+
+            left = 0;
+            // calculate the position for each item
+            for (idx = firstRows[row]; idx < nextRowFirstIdx; idx += 1) { // re-iterate row elements
+                top = height + ~~((maxHeight - itemInfo[idx].height) / 2);
+                lineup[idx] = {
+                    'top': top + 'px',
+                    'left': ~~left + 'px'
+                };
+                left += itemInfo[idx].width + rowMarginX;
+            }
+
+            height += maxHeight + settings.marginY;
+            rowWidth = 0;
+            maxHeight = 0;
+        }
+        lineup.height = height - settings.marginY;
+        return lineup;
+    }
+
+
     $.fn.justify = function (options) {
 
         // Create some defaults, extending them with any options that were provided
@@ -51,7 +179,9 @@
         item = undefined; // free for GC
 
         function justify() {
-            var x = that.width();
+            var x = that.width(),
+                lineup,
+                idx;
 
             if (x === rowMaxWidth) { // width has not changed => nothing needs to be done
                 return false;
@@ -59,133 +189,10 @@
                 rowMaxWidth = x;
             }
 
-            /**
-             * Returns an array of the indices of the items which are the first in each row if the rows are breaking at
-             * the given maximum width. The length of the array is the number of rows which are needed.
-             *
-             * @param maxWidth
-             * @return {Array}
-             */
-            function getRowFirstItems(maxWidth) {
-                if (itemLength === 0) {
-                    return [];
-                }
-                var rowFirstItems = [ 0 ],
-                    i,
-                    rowWidth = 0,
-                    itemStored = true, // current item index is already stored in the array rowFirstItems
-                    itemWidth;
-                for (i = 0; i < itemLength; i += 1) { // iterate all items
-                    itemWidth = itemInfo[i].width;
-                    rowWidth += itemWidth;
-                    if (rowWidth > maxWidth) { // current item breaks max row width => break row
-                        if (itemStored) {
-                            rowWidth = 0;
-                        } else {
-                            rowFirstItems.push(i);
-                            rowWidth = itemWidth + settings.marginX;
-                        }
-                    } else {
-                        rowWidth += settings.marginX;
-                    }
-                    itemStored = false;
-                }
-                return rowFirstItems;
-            }
-
-            /**
-             * Returns an array of the same semantic and length as getRowFirstItems() but the items are sectioned in
-             * an optimized way.
-             * The optimization tries to arrange the items in a more even way (uses binary search to come to a result
-             * fast)
-             *
-             * @param maxWidth
-             * @return {Array}
-             */
-            function getRowFirstItemsOptimized(maxWidth) {
-                var dif = maxWidth,
-                    firstBest = getRowFirstItems(maxWidth),
-                    firstBestWidth = maxWidth,
-                    firstCurrent = firstBest,
-                    i;
-                for (i = 0; i < settings.accuracy; i += 1) {
-                    dif /= 2;
-                    if (firstCurrent.length > firstBest.length) { // needed more rows => increase width
-                        maxWidth += dif;
-                    } else {
-                        if (firstBestWidth > maxWidth) {
-                            firstBest = firstCurrent;
-                            firstBestWidth = maxWidth;
-                        }
-                        maxWidth -= dif;
-                    }
-                    firstCurrent = getRowFirstItems(maxWidth);
-                }
-                return firstBest;
-            }
-
-            /**
-             *
-             *
-             * @param items
-             * @param maxWidth
-             * @return {Array}
-             */
-            function createLineUp(items, maxWidth) {
-                var firstRows = getRowFirstItemsOptimized(maxWidth),
-                    row,
-                    col,
-                    idx,
-                    nextRowFirstIdx,
-                    rowWidth = 0,
-                    maxHeight = 0,
-                    rowMarginX,
-                    height = 0,
-                    rowLength = firstRows.length,
-                    lineup = [],
-                    top,
-                    left;
-
-                lineup.length = itemLength; // expand array with undefined elements to known length (performance)
-
-                for (row = 0, idx = 0; row < rowLength; row += 1) { // iterate rows
-                    nextRowFirstIdx = firstRows[row + 1];
-                    if (nextRowFirstIdx === undefined) {
-                        nextRowFirstIdx = itemLength;
-                    }
-                    for (col = 0; idx < nextRowFirstIdx; idx += 1, col += 1) { // iterate row elements
-                        // maxHeight: maximum height of all items in current row (type int)
-                        maxHeight = Math.max(itemInfo[idx].height, maxHeight);
-                        // rowWith: summed width of all items in current row (type int)
-                        rowWidth += itemInfo[idx].width;
-                    }
-                    // calculate marginX value for current row to stretch the row to the container width (type float)
-                    rowMarginX = ((rowMaxWidth - rowWidth) / (col - 1));
-
-                    left = 0;
-                    for (idx = firstRows[row]; idx < nextRowFirstIdx; idx += 1) { // re-iterate row elements
-                        top = height + ~~((maxHeight - itemInfo[idx].height) / 2);
-                        lineup[idx] = {
-                            'top': top + 'px',
-                            'left': ~~left + 'px'
-                        };
-                        left += itemInfo[idx].width + rowMarginX;
-                    }
-
-                    height += maxHeight + settings.marginY;
-                    rowWidth = 0;
-                    maxHeight = 0;
-                }
-                lineup.height = height - settings.marginY;
-                return lineup;
-
-            }
-
-            var lineup = createLineUp(items, rowMaxWidth);
+            lineup = createLineUp(itemInfo, rowMaxWidth, settings);
 
             settings.onChangeHeight(lineup.height);
 
-            var idx;
             for (idx = 0; idx < itemLength; idx += 1) {
                 itemInfo[idx].item.css(lineup[idx]);
             }
@@ -198,6 +205,6 @@
         while (justify()) {} // do twice if scroll bar appeared
 
         return this;
-
     };
+
 })(jQuery);
